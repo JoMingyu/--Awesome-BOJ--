dd31e0d242c5d755ea66dbbab9eb3db075bda2b4
JoMingyu <city7310@naver.com>
2018-05-09 22:52:30 +0900

    10950

---
+++ 10950: A+B - 3/main.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/10950
+
+# [input]
+# 5
+# 1 1
+# 2 3
+# 3 4
+# 9 8
+# 5 2
+
+# [output]
+# 2
+# 5
+# 7
+# 17
+# 7
+
+print(*[(lambda a, b: a + b)(*map(int, input().split(','))) for _ in range(int(input()))], sep='\n')
